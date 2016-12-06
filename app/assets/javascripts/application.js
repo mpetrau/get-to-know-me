f447aea9c673687aa37ee4591aeb448e6b575512
Jessica Smith <adamcarabetta@gmail.com>
2016-12-06 13:04:46 +0100

    Re-ordered the importing of stylesheets and javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
 //= require flat-ui/dist/js/vendor/video
 //= require flat-ui/dist/js/flat-ui
+//= require_tree .
+
 
 
