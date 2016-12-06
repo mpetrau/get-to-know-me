f5189ee3b455709634efdb594949bc4cda2fdd1f
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-12-06 18:37:51 +0100

    finished Happy Flow for from user traits

--- app/controllers/user_traits_controller.rb
+++ app/controllers/user_traits_controller.rb
@@ -6,9 +6,6 @@ class UserTraitsController < ApplicationController
   end
 
   def create
-    # p user_traits_params[:traits]
-    # puts "-------------------------"
-    # p var = user_traits_params[:traits].drop(1)
 
     user = current_user
     new_traits = user_traits_params[:traits].drop(1)
@@ -19,7 +16,7 @@ class UserTraitsController < ApplicationController
         render :index
       end
     end
-    redirect_to "/"
+    redirect_to offers_path
     flash[:notice] = "We learned about you!"
   end
 
