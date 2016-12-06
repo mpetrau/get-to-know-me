30732d1ca469f42da86b50cee2ec14bd5594af9b
Matilda Waters <matilda.waters1@gmail.com>
2016-12-06 17:00:51 +0000

    feedback view done basic structure

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -3,8 +3,25 @@ class OffersController < ApplicationController
   end
 
   def edit
+    @offer= Offer.find(params[:id])
   end
 
   def update
+    @offer= Offer.find(params[:id])
+    if params[:bought?]
+      @offer.update(:bought? => true)
+    elsif params[:liked?]
+      @offer.update(:liked? => true)
+    elsif params[:disliked?] 
+      @offer.update(:disliked? => true)
+    end
+    redirect_to offers_path
   end
+  
+  private
+
+  def offer_params
+    params.require(:offer).permit([:bought?, :liked?, :disliked?])
+  end
+
 end
