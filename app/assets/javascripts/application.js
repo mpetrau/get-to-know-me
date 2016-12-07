8e067b71182f684eb94bbaefd316daaa7da79619
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-12-07 16:11:33 +0100

    fixed style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,3 @@
 //= require flat-ui/dist/js/vendor/video
 //= require flat-ui/dist/js/flat-ui
 //= require_tree .
-
-        // alert("you have to select at least 2");
-        // $("body").html(<%= flash[:alert] = "There is a mistake submitting your form" %>);
