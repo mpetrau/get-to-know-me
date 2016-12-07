7057eca0bde1ccce020a5a26d8f74c89070502c9
Matilda Waters <matilda.waters1@gmail.com>
2016-12-07 15:52:56 +0000

    card reverse done but not yet rendered in index

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,11 @@
 class OffersController < ApplicationController
   def index
+    @offers= Offer.all
   end
+  
+  # def show
+  #   @offer= Offer.find(params[:id])
+  # end
 
   def edit
     @offer= Offer.find(params[:id])
