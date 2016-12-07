b4f9876ba621037729c5a16e97e09f4298576503
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-12-07 22:45:30 +0100

    created methods to updated offers list upon inserting a new DealTag

--- app/models/deal_tag.rb
+++ app/models/deal_tag.rb
@@ -1,25 +1,29 @@
 class DealTag < ApplicationRecord
-  after_commit :generate_offers
+  after_save :generate_offers
 
   belongs_to :deal
   belongs_to :tag
 
   validates :deal_id, :tag_id, presence: :true
 
-  # def generate_offers
-  #   if Offer.where("deal_id": :deal_id).empty?
-  #     create_offers
-  #   else
-  #     update_offers
-  #   end
-  # end
+  def generate_offers
+    @deal = DealTag.last.deal
+    if Offer.where(deal: @deal).empty?
+      create_offers
+    else
+      update_offers
+    end
+  end
 
-  # private
+  private
 
-  # def create_offers
+  def create_offers
+    User.all.each do |user|
+      Offer.create(deal: @deal, user: user, score: Offer::score(user, @deal))
+    end
+  end
 
-  # end
-
-  # def update_offers
-  # end
+  def update_offers
+    Offer.where(deal: @deal).each { |offer| offer.update(score: Offer::score(offer.user, @deal)) }
+  end
 end
