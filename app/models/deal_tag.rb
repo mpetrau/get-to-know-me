593157c46eca6210d3475709cd0445346d858263
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-12-08 14:20:44 +0100

    enambed access to Services from DealTag nodel

--- app/models/deal_tag.rb
+++ app/models/deal_tag.rb
@@ -1,9 +1,12 @@
 class DealTag < ApplicationRecord
-  after_commit :generate_offers
+  after_commit :generator
 
   belongs_to :deal
   belongs_to :tag
 
   validates :deal_id, :tag_id, presence: :true
 
+  def generator
+    GenerateOffers.generate_deal_offers
+  end
 end
