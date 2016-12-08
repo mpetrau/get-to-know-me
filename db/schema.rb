dc5f5887fb1dd4f4d749d6820daa9854e3e9f98d
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-12-08 17:15:20 +0100

    Revert "Revert "added pictures to cards and to the seed file""
    This reverts commit 2e947b66d94d77db53daea7adb7013390f960ee2.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207215012) do
+ActiveRecord::Schema.define(version: 20161208144752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161207215012) do
     t.string   "provider"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "photo"
   end
 
   create_table "offers", force: :cascade do |t|
