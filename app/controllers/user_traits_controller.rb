c43f91d9a5f17e453556c51e853d121de2416e97
Matilda Waters <matilda.waters1@gmail.com>
2016-12-09 15:57:56 +0000

    profile page in progress

--- app/controllers/user_traits_controller.rb
+++ app/controllers/user_traits_controller.rb
@@ -3,10 +3,11 @@ class UserTraitsController < ApplicationController
   def index
     @traits = Trait.all
     @user_trait = UserTrait.new
+    @user= current_user
   end
 
   def create
-    @new_traits = user_traits_params[:traits].drop(1)
+    @new_traits = user_traits_params[:trait_ids].drop(1)
     @new_traits.each do |trait|
       new_trait = current_user.user_traits.build(trait_id: trait)
       unless new_trait.save
@@ -19,15 +20,16 @@ class UserTraitsController < ApplicationController
     redirect_to offers_path
   end
   
-  def edit
-    @traits= Trait.all
-    @user_trait = UserTrait.new
-  end
+  # def edit
+  #   @traits= Trait.all
+  #   @user_trait = UserTrait.new
+  # end
   
-  def update
-    @user_trait = UserTrait.find(params[:id_trait].to_i)
-    @user_trait.update(user_traits_params)
-  end
+  # def update
+  #   @user_trait = UserTrait.find(params[:id_trait].to_i)
+  #   @user_trait.update(user_traits_params)
+  #   redirect_to profile_path
+  # end
   
   def destroy
     UserTrait.find(params[:id_trait].to_i).destroy
@@ -37,7 +39,7 @@ class UserTraitsController < ApplicationController
   private
 
   def user_traits_params
-    params.require(:user).permit(traits: [])
+    params.require(:user).permit(trait_ids: [])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "registrations"  }
   resources :offers, only: [:index, :edit, :update, :show]
   resources :favorites, only: [:index], to: 'offers#favorites'
-  resources :user_traits, only: [:index, :create, :edit, :update]
+  resources :user_traits, only: [:index, :create]
   resources :profiles, only: [:show]  do
     delete "delete/:id_trait" , to: "user_traits#destroy", as: "del_trait"
   end
