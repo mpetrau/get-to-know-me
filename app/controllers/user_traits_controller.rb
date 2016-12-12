9ba9a771240eef9211d9414543383d6bdd3317e2
Matilda Waters <matilda.waters1@gmail.com>
2016-12-12 11:29:00 +0000

    finally it workedgit add .!

--- app/controllers/user_traits_controller.rb
+++ app/controllers/user_traits_controller.rb
@@ -6,30 +6,16 @@ class UserTraitsController < ApplicationController
     @user= current_user
   end
 
-  def create
-    @new_traits = user_traits_params[:trait_ids].drop(1)
+  def update
+    current_user.user_traits.destroy_all
+    @new_traits = user_traits_params[:trait_ids]
     @new_traits.each do |trait|
-      new_trait = current_user.user_traits.build(trait_id: trait)
-      unless new_trait.save
-        flash[:alert] = "There is a mistake submitting your form"
-        render :index
-      end
-      end
+      new_trait = current_user.user_traits.create(trait_id: trait)
+    end
     GenerateOffers.generate_user_offers(current_user)
     flash[:notice] = "Thanks for sharing. We used it to target offers for you."
     redirect_to offers_path
   end
-  
-  # def edit
-  #   @traits= Trait.all
-  #   @user_trait = UserTrait.new
-  # end
-  
-  # def update
-  #   @user_trait = UserTrait.find(params[:id_trait].to_i)
-  #   @user_trait.update(user_traits_params)
-  #   redirect_to profile_path
-  # end
   
   def destroy
     UserTrait.find(params[:id_trait].to_i).destroy
