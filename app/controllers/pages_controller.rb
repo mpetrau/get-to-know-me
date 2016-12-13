b0de9d3e13acf0b4cb91ea2f4aa66be722a26ea8
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-12-13 17:14:45 +0100

    guest user ready to ship

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :privacy ]
+  skip_before_action :authenticate_user!, only: [ :home, :privacy]#, :hellobug ]
 
   def home
     # @displayable_nav = true
   end
+
+  # def hellobug
+  #   redirect_to '/user_traits'
+  # end
 
   def privacy
   end
