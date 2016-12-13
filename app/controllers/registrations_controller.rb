b5ed428bbf296e36b8a3fff943a15b6e3407da0b
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-12-13 12:48:29 +0100

    created custom registrations routes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
+skip_before_action :authenticate_user!, :only => [:edit, :update]
+
+  def edit
+    byebug
+    sign_in :user, @temp_user if temp_user?
+    super
+  end
 
   def update
-    sign_in :user, @temp_user if temp_user?
     super
     redirect_to edit_user_registration_path if temp_user?
     self.user_type = "member" if self.user_type == "guest"
   end
 
   protected
+
+  def resource_name
+    "user"
+  end
+
+  def set_minimum_password_length
+    6
+  end
 
   def after_sign_up_path_for(resource)
     user_traits_path
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,20 @@ Rails.application.routes.draw do
   # end
   root 'pages#home'
   get 'pages/privacy'
+
+  get :users, as: :edit_user_registration, controller: :registrations, action: :edit
+  patch :users, as: "", controller: :registrations, action: :update
+
+
   devise_for :users,
-  controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "registrations"  }
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks'  }
   resources :offers, only: [:index, :edit, :update, :show]
   resources :favorites, only: [:index], to: 'offers#favorites'
   resources :user_traits, only: [:index, :update]
   resources :profiles, only: [:show]  do
     delete "delete/:id_trait" , to: "user_traits#destroy", as: "del_trait"
   end
+
 
 
 
