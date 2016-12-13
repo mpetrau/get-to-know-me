389fce26f607c6f955646bcb25eeadc256328343
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2016-12-13 19:29:11 +0100

    added JS to make reverse side button active

--- app/assets/javascripts/offer.js
+++ app/assets/javascripts/offer.js
@@ -18,21 +18,32 @@ $( ".card" ).hover(
   });
 
 // 3. On a card image, go to the deal website and flip Feedback view
-$( ".card-image" ).on('click', function() {
-  if ($(this).next('.feedback.hidden').length){
-    $(this).next(".feedback" ).removeClass( "hidden" );
-    var url = $(this).siblings(".card-link").text();
-    window.open(url, '_blank');
-        // created the effect of muted background for a picture
-        $(this).addClass( "subdue" );
-        // hide card menu for a card which shows Feedback form
-        $(this).next(".feedback").next(".offer-actions" ).hide();
-        // disable card reverse side for a card which shows Feedback form
-        $(this).siblings(".card-content" ).children().addBack().removeClass('activator');
-        return false;
-      }
+$( ".card-image, .btn-2see-deal" ).on('click', function() {
+  // on either selector, reset the starting traversing element to "card-image"
+  if ($(this).hasClass("btn-2see-deal")) {
+    var card_image = $(this).closest(".card-reveal").siblings(".card-image");
+  }
+  else {
+    var card_image = $(this);
+  };
+  // start traversal and execute function
+  if ($(card_image).next('.feedback.hidden').length){
+    $(card_image).next(".feedback" ).removeClass( "hidden" );
+      // open URL from a card
+      var url = $(card_image).siblings(".card-link").text();
+      window.open(url, '_blank');
+      // created the effect of muted background for a picture
+      $(card_image).addClass( "subdue" );
+      // hide card menu for a card which shows Feedback form
+      $(card_image).next(".feedback").next(".offer-actions" ).hide();
+      // hide the card reverse, if the routing action was from btn-2see-deal
+      $(card_image).siblings(".card-reveal").hide();
+      // disable card reverse side for a card which shows Feedback form
+      $(card_image).siblings(".card-content" ).children().addBack().removeClass('activator');
+      return false;
+    }
       else
-        $(this).next(".feedback").addClass( "hidden" );
+        $(card_image).next(".feedback").addClass( "hidden" );
     });
 
 // 4. To enable card 'flip' from multiple areas
