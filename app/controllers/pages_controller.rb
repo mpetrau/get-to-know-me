24d60476952fc5e2915d61ccedb8a1f880fd6c54
Jessica Smith <adamcarabetta@gmail.com>
2016-12-13 12:23:37 +0100

    Changed color on How it works Banner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :privacy ]
 
   def home
-    @disable_nav = true
+    # @displayable_nav = true
   end
 
   def privacy
